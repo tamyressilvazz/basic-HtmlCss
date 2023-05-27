5c83ec4eaf36ee3a43e3a0a8a3922ca343afa752
Tamyres <74621120+tamyressilvazz@users.noreply.github.com>
2023-05-27 15:27:37 -0300

    deleted js itens study

--- Alura/arrow-function.js
+++
@@ -1,8 +0,0 @@
-function apresentando(nome) {
-    return `my name is ${nome}`;
-}
-
-let apresentacao = nome => `My name is ${nome}`;
-
-console.log(apresentacao('Carla'));
-console.log(apresentacao('Dina'));
